fe0ad775af8f05b9b9964a1b44a224b3f20c912f
frandimitri <dimitrifranco1@gmail.com>
2025-05-18 19:02:21 -0300

    subida inicial del proyecto para comenzar a trabajarlo

---
+++ src/main/java/ong_desi/DesiTp3Application.java
@@ -0,0 +1,13 @@
+package ong_desi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesiTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesiTp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ong_desi/DesiTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ong_desi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesiTp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
