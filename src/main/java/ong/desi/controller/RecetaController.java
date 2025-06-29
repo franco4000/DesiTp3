866c34691f4bb97dc4a49a3cfc2d0432cb9f2bff
Oliver figueroa <oliverfigueroa101092@gmail.com>
2025-06-29 19:01:32 -0300

    Fix: Corrige mapeo ambiguo en RecetaController

--- src/main/java/ong/desi/controller/RecetaController.java
+++ src/main/java/ong/desi/controller/RecetaController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/recetas")
+@RequestMapping("/api/recetas")
 @CrossOrigin(origins = "*")
 public class RecetaController {
 
